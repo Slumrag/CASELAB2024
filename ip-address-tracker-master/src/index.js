b9b0514d3792c82d4e3bac9913a76e68db8df36b
Slumrag <mrgreenthecristal@yandex.ru>
2024-10-13 22:00:18 +0300

    feat: update info div after successful API request

--- ip-address-tracker-master/src/index.js
+++ ip-address-tracker-master/src/index.js
@@ -2,14 +2,26 @@ import { getGeolocationByIp } from './api/getGeolocationByIp';
 import { validateIp } from './utils';
 
 const searchBar = document.querySelector('.search-bar');
+const ipAddress = document.querySelector('#ip');
+const location = document.querySelector('#location');
+const timezone = document.querySelector('#timezone');
+const isp = document.querySelector('#isp');
 
 searchBar.addEventListener('submit', handleIpSubmit);
+
+function setGeolocationInfo(info) {
+  ipAddress.textContent = info.ip;
+  const { country, region } = info.location;
+  location.textContent = `${country} ${region}`;
+  timezone.textContent = info.location.timezone;
+  isp.textContent = info.isp;
+}
 
 function handleIpSubmit(e) {
   e.preventDefault();
   const ip = e.target.ipAddress.value;
   if (validateIp(ip)) {
-    getGeolocationByIp(ip).then(console.log);
+    getGeolocationByIp(ip).then(setGeolocationInfo);
   } else {
     alert('Invalid IP address');
   }
