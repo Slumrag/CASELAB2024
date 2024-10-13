e464901e988087bba0b414baacfcb7c8e3e6112d
Slumrag <mrgreenthecristal@yandex.ru>
2024-10-13 21:44:07 +0300

    feat: add ip address validation

--- ip-address-tracker-master/src/index.js
+++ ip-address-tracker-master/src/index.js
@@ -1,22 +1,16 @@
-const ipInput = document.querySelector('.search-bar__input');
-const ipSubmit = document.querySelector('.search-bar__btn');
+import { getGeolocationByIp } from './api/getGeolocationByIp';
+import { validateIp } from './utils';
 
-ipSubmit.addEventListener('click', handleIpSubmit);
-ipInput.addEventListener('keydown', handleIpInputKeyDown);
+const searchBar = document.querySelector('.search-bar');
 
-async function getGeolocationByIp(ip) {
-  const apiURL = new URL('https://geo.ipify.org/api/v2/country');
-  apiURL.searchParams.set('ipAddress', ip);
-  apiURL.searchParams.set('apiKey', process.env.API_KEY_GEOLOCATION);
-
-  const response = await fetch(apiURL);
-
-  return response.json();
-}
+searchBar.addEventListener('submit', handleIpSubmit);
 
 function handleIpSubmit(e) {
-  const ip = ipInput.value;
-  getGeolocationByIp(ip).then(console.log);
+  e.preventDefault();
+  const ip = e.target.ipAddress.value;
+  if (validateIp(ip)) {
+    getGeolocationByIp(ip).then(console.log);
+  } else {
+    alert('Invalid IP address');
+  }
 }
-
-function handleIpInputKeyDown(params) {}
